1e58a9cb6bcffefce783f5e074a5697581dbbb48
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2024-06-21 17:27:38 +0530

    commit 10

--- src/nathan/settings.py
+++ src/nathan/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_3)dy+h+*8#sq6n*k(8(cj6^#+5lv_ls+n07=ex31e^#o59ntt'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
@@ -87,6 +87,15 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+CONN_MAX_AGE =config("CONN_MAX_AGE" ,cast=int , default=30)
+
+DATABASE_URL =config("DATABASE_URL" ,cast=str)
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default':dj_database_url.config(default=DATABASE_URL, conn_max_age=CONN_MAX_AGE,conn_health_checks=True) 
+}
+
 
 
 # Password validation
