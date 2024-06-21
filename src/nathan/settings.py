8d71f982668fa91ca41695297202d00d77ef57a7
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2024-06-21 12:26:45 +0530

    commit 4

--- src/nathan/settings.py
+++ src/nathan/settings.py
@@ -24,8 +24,14 @@ SECRET_KEY = 'django-insecure-_3)dy+h+*8#sq6n*k(8(cj6^#+5lv_ls+n07=ex31e^#o59ntt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app" #https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS+=[
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
--- src/nathan/views.py
+++ src/nathan/views.py
@@ -4,46 +4,30 @@ from django.http import HttpResponse
 from visits.models import PageVisit
 this_dir=pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request , *args ,**kwargs):
+def home_view(request , *args ,**kwargs):
+    return about_view(request, *args ,**kwargs)
+
+
+
+def about_view(request , *args ,**kwargs):
     qs=PageVisit.objects.all()
     page_qs= PageVisit.objects.filter(path=request.path)
+    try:
+        percent=page_qs.count()*100.0/qs.count()
+    except:
+        percent=0
     print(this_dir)
     my_title="My page"
     my_context={
      "page_title":my_title,
      "page_visit_count":page_qs.count(),
-     "percent":page_qs.count()*100.0/qs.count(),
+     "percent":percent,
      "total_visit_count":qs.count()
     }
     path=request.path
     html_template="home.html"
     PageVisit.objects.create(path=request.path)
     return render(request,html_template , my_context) 
-
-
-
-def my_old_home_page_view(request , *args ,**kwargs):
-    print(this_dir)
-    my_title="Mypage"
-    my_context={"page_title":my_title}
-    html_="""
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-  <body>
-    <h1>{page_title}anything this anything?</h1>
-  </body>
-</html>
-
-""".format(**my_context)
-   # html_file_path=this_dir/"home.html"
-    #html_=html_file_path.read_text()
-    return HttpResponse(html_) 
-
 
 
 
